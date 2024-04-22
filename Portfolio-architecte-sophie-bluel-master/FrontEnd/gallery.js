e6f38aaa274a11e498201416d654b8f731ba886e
Jorane Saubesty <jorane.saubesty@gmail.com>
2024-04-22 21:08:19 +0300

    Rename gallery.js and generate dynamic gallery from API

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/gallery.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/gallery.js
@@ -1,9 +1,6 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const gallery = await reponse.json();
 
-
-
-
 function generateGallery(gallery) {
     for (let i = 0; i < gallery.length; i++) {
 
@@ -16,6 +13,8 @@ function generateGallery(gallery) {
         const imageElement = document.createElement("img");
         // Get the image from the API
         imageElement.src = article.imageUrl;
+        // Get the alternative element from the API
+        imageElement.alt = article.title ?? "no description available";
         // Create the caption
         const captionFigure = document.createElement("figcaption");
         // Get the caption from the API
