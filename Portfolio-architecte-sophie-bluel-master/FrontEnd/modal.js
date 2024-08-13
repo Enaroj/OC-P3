f338c79f84be9f1590a1045c2fc6c1f5b1119958
Jorane Saubesty <jorane.saubesty@gmail.com>
2024-08-13 18:19:33 +0300

    Send item to API

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/modal.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/modal.js
@@ -1,8 +1,9 @@
 import { initData } from "./gallery-login.js"
 //import { getToken } from "./login.js"
+import { getToken } from "./gallery-login.js"
 
 // This function manage the global behavior of the modal box, using keyboard or mouse
-function modalOpenClose() {
+function initModal() {
 
     let modal = null
     // Create a selector to know all the selectionable elements
@@ -13,10 +14,11 @@ function modalOpenClose() {
     let previouslyFocusElement = null
 
     // This constant manage the opening of the modal box
+    
     const openModal = function (event) {
         event.preventDefault()
         console.log("le lien a été cliqué")
-        initiateModalContent()
+        initModalContent()
         // Fetch the element "href" in the page
         modal = document.querySelector(event.target.getAttribute("href"))
         // Select all the element focusable that are into the modal box and put in a table
@@ -107,105 +109,12 @@ function modalOpenClose() {
         }
     })
 
+
+
 }
 
-function setAddPhotoModal() {
-    // Listening to the "Ajouter une photo" button inside the modal box
-    const addButton = document.querySelector("#btn-add")
-    addButton.addEventListener("click", function (event) {
-        event.preventDefault()
-        console.log("le bouton ajouter a été cliqué");
-
-        // Changing the title of the modal box
-        document.querySelector("h3").innerText = "Ajout photo";
-        // Changing the id of the div of the inside content of the modal box
-        document.querySelector(".inner-modal").id = "add-photo-modal";
-        // Emptying the HTML of that div
-        document.querySelector(".inner-modal").innerHTML = ""
-
-        // Revealing the back button
-        const hideButton = document.querySelector(".fa-arrow-left");
-        hideButton.style.display = null
-        // Listening to the click on that button
-        hideButton.addEventListener("click", function () {
-            console.log("le bouton retour a été cliqué");
-            // Recreating the first window of the modal box
-            initiateModalContent();
-        })
-
-        // Changing the content and idea of the bottom button
-        addButton.innerText = "Valider";
-        addButton.id = "btn-ok";
-
-        // Creating the content of the second window of the modal box
-        const sectionAddPhoto = document.querySelector("#add-photo-modal");
-        console.log(sectionAddPhoto)
-        //if (sectionAddPhoto.firstChild) {
-            
-            // Create the div element to contain the first part of the window
-            const divAddPhoto = document.createElement("div");
-            divAddPhoto.classList.add('div-add-photo');
-            const imageIcon = document.createElement("i");
-            imageIcon.classList.add('fa-regular', 'fa-image');
-
-            // Create a label element to contain a span and an input in order to upload a new photo. 
-            // The input is hidden and the span is set to look like a button while acting like an input. 
-            const addPhotoLabel = document.createElement("label");
-            addPhotoLabel.for = "images";
-            const addPhotoButton = document.createElement("span");
-            addPhotoButton.innerText = " + Ajouter photo";
-            // Setting the input
-            const addPhotoInput = document.createElement("input");
-            addPhotoInput.type = "file";
-            addPhotoInput.accept = ".jpg, .png";
-            addPhotoInput.id = "images";
-
-            // Adding a description about what extensions are supported by the input
-            const addPhotoText = document.createElement("p");
-            addPhotoText.innerText =  "jpg, png : 4 mo max"
-
-            // Creating all the elements in the DOM
-            sectionAddPhoto.appendChild(divAddPhoto);
-            divAddPhoto.appendChild(imageIcon);
-            divAddPhoto.appendChild(addPhotoLabel);
-            addPhotoLabel.appendChild(addPhotoButton)
-            addPhotoLabel.appendChild(addPhotoInput);
-            divAddPhoto.appendChild(addPhotoText);
-
-            // Adding a second form part of the window in order to categories the photo uploaded
-            const formAddPhoto = document.createElement("form");
-            formAddPhoto.method = "post";
-    
-            sectionAddPhoto.appendChild(formAddPhoto);
-
-            // Adding a title part
-            const photoTitlelabel = document.createElement("label");
-            photoTitlelabel.innerText = "Titre";
-            const photoTitle = document.createElement("input");
-            photoTitle.type = "text"
-            photoTitle.name = "title-photo"
-            photoTitle.id = "title-photo"
-    
-            formAddPhoto.appendChild(photoTitlelabel);
-            formAddPhoto.appendChild(photoTitle);
-
-            // Adding a category part
-            const photoCatlabel = document.createElement("label");
-            photoCatlabel.innerText = "Catégorie";
-            const photoCat = document.createElement("input");
-            photoCat.type = "text"
-            photoCat.name = "cat-photo"
-            photoCat.id = "cat-photo"
-    
-            formAddPhoto.appendChild(photoCatlabel);
-            formAddPhoto.appendChild(photoCat);
-        //} 
-
-    })
-}
-
-function initiateModalContent() {
-    console.log("la fonction initiateModal est lancée")
+function initModalContent() {
+    console.log("la fonction initModal est lancée")
 
     // Change the title of the modal box
     document.querySelector("h3").innerText = "Galerie photo";
@@ -213,29 +122,308 @@ function initiateModalContent() {
     document.querySelector(".inner-modal").id = "gallery-modal";
     // Empty the div from previous content
     document.querySelector(".inner-modal").innerHTML = "";
-    
-    // Checking what should I call in the if section below.
-    const div = document.querySelector(".inner-modal")
-    const divHTML = div.innerHTML
-    console.log(div);
-    console.log(divHTML)
-    // Generate the modal gallery if the div is empty
-    if (divHTML === "") {
-        initData()
-    }
+
+
+
+    //Generate the modal gallery 
+    initData();
 
     // Hide the return button
     const hideButton = document.querySelector(".fa-arrow-left");
     hideButton.style.display = "none";
 
-    // Changing the text of the bottom button if it's not changed yet.
-    const addButton = document.querySelector("#btn-ok");
-    if (addButton !== null) {
-        addButton.innerText = "Ajouter une photo";
-        addButton.id = "btn-add";
-    } 
+    // Reveiling a navigation button part
+    document.querySelector("#btn-add").style.display = null;
+    document.querySelector("hr").style.display = null;
+
+    setAddPhotoModal();
+}
+
+function changeTitle() {
+    // Changing the title of the modal box
+    document.querySelector("h3").innerText = "Ajout photo";
+    // Changing the id of the div of the inside content of the modal box
+    document.querySelector(".inner-modal").id = "add-photo-modal";
+    // Emptying the HTML of that div
+    document.querySelector(".inner-modal").innerHTML = "";
+}
+
+function showBackButton() {
+    // Revealing the back button
+    const hideButton = document.querySelector(".fa-arrow-left");
+    hideButton.style.display = null
+    // Listening to the click on that button
+    hideButton.addEventListener("click", function () {
+        console.log("le bouton retour a été cliqué");
+        // Recreating the first window of the modal box
+        initModalContent();
+    });
 
 }
 
-modalOpenClose(initData())
-setAddPhotoModal()
+function addNewPhoto(formAddPhoto) {
+    // Create the div element to contain the first part of the window : input file
+    const divAddPhoto = document.createElement("div");
+    divAddPhoto.classList.add('div-add-photo');
+    // Create the icon "image"
+    const imageIcon = document.createElement("i");
+    imageIcon.classList.add('fa-regular', 'fa-image');
+    // Create a label element to contain a span and an input in order to upload a new photo. 
+    // The input is hidden and the span is set to look like a button while acting like an input. 
+    const addPhotoLabel = document.createElement("label");
+    addPhotoLabel.for = "images";
+    addPhotoLabel.id = "label-image"
+    const addPhotoButton = document.createElement("span");
+    addPhotoButton.innerText = " + Ajouter photo";
+    // Setting the input
+    const addPhotoInput = document.createElement("input");
+    addPhotoInput.type = "file";
+    addPhotoInput.accept = ".jpg, .png";
+    addPhotoInput.id = "images";
+    // Adding a description about what extensions are supported by the input
+    const addPhotoText = document.createElement("p");
+    addPhotoText.innerText = "jpg, png : 4 mo max";
+    addPhotoText.id = "p-image"
+
+    // Create a div into the form to manage the input type file
+    formAddPhoto.appendChild(divAddPhoto);
+    // Adding the icon
+    divAddPhoto.appendChild(imageIcon);
+    // Adding the label, button, input and description text
+    divAddPhoto.appendChild(addPhotoLabel);
+    addPhotoLabel.appendChild(addPhotoButton);
+    addPhotoLabel.appendChild(addPhotoInput);
+    divAddPhoto.appendChild(addPhotoText);
+
+
+}
+
+function addPhotoTitle(formAddPhoto) {
+    // Adding a title part
+    const photoTitlelabel = document.createElement("label");
+    photoTitlelabel.innerText = "Titre";
+    const photoTitle = document.createElement("input");
+    photoTitle.type = "text";
+    photoTitle.name = "title-photo";
+    photoTitle.id = "title-photo";
+
+    // Adding the title field
+    formAddPhoto.appendChild(photoTitlelabel);
+    formAddPhoto.appendChild(photoTitle);
+
+}
+
+function addPhotoCategory(formAddPhoto) {
+    // Adding a category part
+    const photoCatlabel = document.createElement("label");
+    photoCatlabel.innerText = "Catégorie";
+    const photoCat = document.createElement("select");
+    photoCat.name = "cat-photo";
+    photoCat.id = "cat-photo";
+    const options = [
+        { value: 'Tous', text: '' },
+        { value: 'Objet', text: 'Objet' },
+        { value: 'Appartements', text: 'Appartements' },
+        { value: 'Hotels-Restaurants', text: 'Hotels & Restaurants' },
+    ];
+    options.forEach(optionData => {
+        const optionElement = document.createElement('option');
+        optionElement.value = optionData.value;
+        optionElement.textContent = optionData.text;
+        photoCat.appendChild(optionElement);
+    });
+
+
+    // Adding the category field
+    formAddPhoto.appendChild(photoCatlabel);
+    formAddPhoto.appendChild(photoCat);
+}
+
+function submitPhotoButton(formAddPhoto) {
+
+    const createLine = document.createElement("hr");
+
+    formAddPhoto.appendChild(createLine);
+
+    const submitButton = document.createElement("input");
+    submitButton.id = "btn-ok";
+    submitButton.type = "submit";
+    submitButton.value = "Valider";
+    submitButton.disabled = true;
+
+    formAddPhoto.appendChild(submitButton);
+
+}
+
+function addForm() {
+
+    // Creating the content of the second window of the modal box
+    const sectionAddPhoto = document.querySelector("#add-photo-modal");
+    console.log(sectionAddPhoto);
+
+    // Adding a form
+    const formAddPhoto = document.createElement("form");
+    formAddPhoto.method = "post";
+    formAddPhoto.id = "my-form";
+    formAddPhoto.action = "/submit";
+
+    addNewPhoto(formAddPhoto);
+    addPhotoTitle(formAddPhoto);
+    addPhotoCategory(formAddPhoto);
+    submitPhotoButton(formAddPhoto);
+
+    // Creating all the elements in the DOM
+    // Create the form
+    sectionAddPhoto.appendChild(formAddPhoto);
+
+}
+
+function setUpFormListener () {
+    const form = document.getElementById('my-form');
+
+    const elementError = document.createElement("p");
+    elementError.id = "add-photo-error-message";
+    form.appendChild(elementError);
+
+    form.addEventListener('change', function() {
+        const fileInput = document.querySelector('input[type="file"]');
+        console.log(fileInput.value)
+        const titleInput = document.getElementById('title-photo');
+        console.log(titleInput.value)
+        const categoryInput = document.querySelector('#cat-photo');
+        console.log(categoryInput.value)
+
+
+        if (fileInput.value != "" && titleInput.value != "" && categoryInput.value != "Tous") {
+            console.log("Tout est en règle vous pouvez circuler");
+            document.getElementById("btn-ok").disabled = false;
+            document.getElementById("btn-ok").id = "btn-ok-valide"; // activer le bouton : done !
+
+        } else {
+            console.log("Vous n'avez pas sélectionné de fichier, ou donné un titre ou sélectionné une catégorie");
+            ;
+            if (document.getElementById("btn-ok-valide") != null) {
+                document.getElementById("btn-ok-valide").id = "btn-ok";
+                document.getElementById("btn-ok").disabled = true;
+            };
+        }
+    });
+
+
+}
+
+function setupSubmitListener() {
+    const form = document.getElementById('my-form') 
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("le bouton submit a été cliqué")
+        addItemtoAPI();      
+    });
+
+}
+
+function displayMiniature() {
+    const image = document.getElementById("images");
+    image.addEventListener('change', (event) => {
+        console.log(event.target.value);
+        const icone = document.querySelector(".fa-image");
+        icone.style.display = "none";
+        const content = document.querySelector(".div-add-photo");
+        const newPhoto = document.createElement("img");
+        newPhoto.id = "preview-photo";
+        content.appendChild(newPhoto);
+        // newPhoto.src = image.files[0];
+
+        document.getElementById("label-image").style.display = "none";
+        document.getElementById("p-image").style.display = "none";
+
+
+        const file = image.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                newPhoto.src = event.target.result;
+                //newPhoto.style.display = "block";
+            };
+            reader.readAsDataURL(file);
+        }
+
+
+
+    });
+}
+
+// Error 400 Bad request
+function addItemtoAPI() {
+    const form = document.getElementById('my-form') // comment ne pas écrire deux fois cette ligne ? (voir setUpFormListener)
+
+
+    
+    
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + getToken());
+
+    const formdata = new FormData();
+    const fileInput = document.querySelector('input[type="file"]');
+    console.log('la valeur de l"input est ', fileInput.value)
+    const file = fileInput.files[0];
+    const titleInput = document.getElementById('title-photo').value;
+    const categoryInput = document.querySelector('#cat-photo').selectedIndex;
+    console.log(categoryInput)
+
+
+    formdata.append("image", file /*, "/path/to/file"*/);
+    formdata.append("title", titleInput);
+    formdata.append("category", categoryInput);
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: formdata,
+        redirect: "follow"
+    };
+
+    fetch("http://localhost:5678/api/works/", requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            console.log(result);
+            //initModal(closeModal());
+            initModalContent();
+            initData();
+        })
+        .catch((error) => {
+            console.error(error);
+            elementError.innerText = "Tous les champs doivent être remplis";
+        })
+}
+
+
+function setAddPhotoModal() {
+
+    const addButton = document.getElementById("btn-add");
+    addButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("le bouton ajouter a été cliqué");
+
+        changeTitle();
+
+        showBackButton();
+
+        document.querySelector("#btn-add").style.display = "none";
+        document.querySelector("hr").style.display = "none";
+
+        addForm();
+
+
+        displayMiniature()
+
+        setUpFormListener ()
+        setupSubmitListener()
+
+    });
+}
+
+
+initModal()
+
